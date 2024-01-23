af6352d41eaccaae4f1adb5a6e73f6c8920a65eb
kevinDsousa <kevindiegodasilvasousa@gmail.com>
2024-01-23 10:50:40 -0300

    chore: feito o header

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -2,9 +2,9 @@ import logo from '../assets/rocket.png'
 
 export const Header = () => {
     return (
-        <header className="w-full flex items-center">
-            <img src={logo} />
-            <p>Todo</p>
+        <header className="w-full h-[200px] flex gap-5 justify-center items-center bg-gray-600">
+            <img className="w-[24px] h-9" src={logo} />
+            <p className='font-inter font-extrabold text-4xl text-blue'>to<span className='text-purple-dark'>do</span></p>
         </header>
     )
 }
